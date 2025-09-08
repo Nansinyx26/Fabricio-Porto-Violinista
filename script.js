39af1ac737efde08775f5bd5d4ed0a9e0f835202
Renan de Oliveira <oliversinyxcontato@gmail.com>
2025-09-08 08:04:49 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ function initDOMCache() {
 const aiKnowledgeBase = {
     precos: {
         question: "Quais são os preços dos serviços?",
-        answer: "Os valores variam de acordo com o tipo de evento e duração. Para casamentos: R$ 1200 (cerimônia), R$ 2000 (cerimônia + coquetel). Para outros eventos, em contato para um orçamento personalizado!"
+        answer: "Os valores variam de acordo com o tipo de evento e duração. Para casamentos: R$ 1200 (cerimônia), R$ 2000 (cerimônia + coquetel). Para outros eventos, entre em contato para um orçamento personalizado!"
     },
     repertorio: {
         question: "Qual é o repertório disponível?",
